653551faa89974129b107e94bf38032b7eb50e09
Xanooo <tamineha@yahoo.fr>
2025-03-26 12:25:54 +0100

    On continue l'ajout de fichier.

--- public/fr.univamu.iut.cart/CartResource.java
+++ public/fr.univamu.iut.cart/CartResource.java
@@ -23,6 +23,45 @@ public class CartResource{
     public CartResource(){}
 
     public CartResource(CartRepositoryInterface cartRepo){
+        this.cartRepo = new CartService(cartRepo);
+    }
+
+    /**
+     *
+     * @param service
+     */
+    public CartResource(CartService service){
+        this.service = service;
+    }
+
+    /**
+     *
+     * @param id
+     * @return
+     */
+    @GET
+    @Path("{id}")
+    @Produces("application/json")
+    public String getCart(@PathParam("id") int id){
+
+        String result = service.getCartJSON(id);
+
+        if result == null throw new NotFoundException();
+
+        return result;
+
+    }
+
+
+    /**
+     *
+     * @param id
+     * @param cart
+     * @return
+     */
+    public Response updateCart(@PathParams("id") int id, Cart cart){
+        if (!service.updateCart(id, cart)) throw new NotFoundException();
+        else return Response.ok("updated").build();
 
     }
 

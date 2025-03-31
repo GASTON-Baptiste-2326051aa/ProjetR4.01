4b83cdf19608979f394a98b42adf4b82c07ce5f3
Titouan <titouan.alvares@etu.univ-amu.fr>
2025-04-01 00:23:54 +0200

    feat: API basique basée uniquement sur la BD accessible (tables 'Order' et 'Order_content')

--- public/order/src/main/java/com/coop/order/Order.java
+++ public/order/src/main/java/com/coop/order/Order.java
@@ -8,17 +8,17 @@ public class Order {
     /**
      * Identifiant de la commande
      */
-    protected int id;
+    protected String id;
 
     /**
      * Identifiant de l'utilisateur
      */
-    protected int userId;
+    protected String userId;
 
     /**
      * Identifiants des paniers
      */
-    protected int[] cartId;
+    protected String[] cartId;
 
     /**
      * Date de la livraison
@@ -31,32 +31,38 @@ public class Order {
     protected String relayAddress;
 
     /**
+     * Si la commande a été validée
+     */
+    protected boolean valid;
+
+    /**
      * Constructeur par défaut
      */
     public Order(){
     }
 
     /**
-     * Constructeur de réservation
+     * Constructeur de commande
      * @param id identifiant de la commande
      * @param userId identifiant de l'utilisateur
-     * @param cartId identifiants des paniers
      * @param date date de la livraison
      * @param relayAddress adresse du relai
+     * @param valid si la commande a été validée
      */
-    public Order(int id, int userId, int[] cartId, String date, String relayAddress) {
+    public Order(String id, String userId, String date, String relayAddress, boolean valid) {
         this.id = id;
         this.userId = userId;
-        this.cartId = cartId;
+        this.cartId = new String[0];
         this.date = date;
         this.relayAddress = relayAddress;
+        this.valid = valid;
     }
 
     /**
      * Méthode permettant d'accéder à l'identifiant de la commande
      * @return l'identifiant de la commande
      */
-    public int getId() {
+    public String getId() {
         return id;
     }
 
@@ -64,7 +70,7 @@ public class Order {
      * Méthode permettant de modifier l'identifiant de la commande
      * @param id un entier contenant l'identifiant à utiliser
      */
-    public void setId(int id) {
+    public void setId(String id) {
         this.id = id;
     }
 
@@ -72,7 +78,7 @@ public class Order {
      * Méthode permettant d'accéder à l'identifiant de l'utilisateur
      * @return l'identifiant de l'utilisateur
      */
-    public int getUserId() {
+    public String getUserId() {
         return userId;
     }
 
@@ -80,7 +86,7 @@ public class Order {
      * Méthode permettant de modifier l'identifiant de l'utilisateur
      * @param userId un entier contenant l'identifiant à utiliser
      */
-    public void setUserId(int userId) {
+    public void setUserId(String userId) {
         this.userId = userId;
     }
 
@@ -88,7 +94,7 @@ public class Order {
      * Méthode permettant d'accéder aux identifiants des paniers
      * @return les identifiants des paniers
      */
-    public int[] getCartId() {
+    public String[] getCartId() {
         return cartId;
     }
 
@@ -96,7 +102,7 @@ public class Order {
      * Méthode permettant de modifier les identifiants des paniers
      * @param cartId un tableau contenant les identifiants à utiliser
      */
-    public void setCartId(int[] cartId) {
+    public void setCartId(String[] cartId) {
         this.cartId = cartId;
     }
 
@@ -133,12 +139,28 @@ public class Order {
     }
 
     /**
+     * Méthode permettant d'accéder à la valeur de l'attribut 'valid'
+     * @return la valeur de 'valid'
+     */
+    public boolean getValid() {
+        return valid;
+    }
+
+    /**
+     * Méthode permettant de valider ou non la commande
+     * @param valid le nouvel état de la commande
+     */
+    public void setValid(boolean valid) {
+        this.valid = valid;
+    }
+
+    /**
      * Méthode mettant tous les identifiants des paniers dans une chaîne de caractères
      * @return une chaîne de caractères contenant les identifiants des paniers
      */
     public String cartIdToString() {
         String cartIdString = "[";
-        for (int id : cartId) {
+        for (String id : cartId) {
             cartIdString += "'" + id + "', ";
         }
         cartIdString = cartIdString.substring(cartIdString.length()-2) + "]";
@@ -157,6 +179,7 @@ public class Order {
                 ", identifiants des paniers=" + cartIdToString() +
                 ", date de livraison='" + date + '\'' +
                 ", adresse du relai='" + relayAddress + '\'' +
+                ", validée=" + valid +
                 '}';
     }
 }
---
+++ public/order/src/main/java/com/coop/order/OrderRepositoryMariadb.java
@@ -0,0 +1,168 @@
+package com.coop.order;
+
+import java.io.Closeable;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ * Classe permettant d'accèder aux commandes stockées dans une base de données Mariadb
+ */
+public class OrderRepositoryMariadb implements OrderRepositoryInterface, Closeable {
+
+    /**
+     * Accès à la base de données (session)
+     */
+    protected Connection dbConnection ;
+
+    /**
+     * Constructeur de la classe
+     * @param infoConnection chaîne de caractères avec les informations de connexion
+     *                       (p.ex. jdbc:mariadb://mysql-[compte].alwaysdata.net/[compte]_library_db
+     * @param user chaîne de caractères contenant l'identifiant de connexion à la base de données
+     * @param pwd chaîne de caractères contenant le mot de passe à utiliser
+     */
+    public OrderRepositoryMariadb(String infoConnection, String user, String pwd ) throws java.sql.SQLException, java.lang.ClassNotFoundException {
+        Class.forName("org.mariadb.jdbc.Driver");
+        dbConnection = DriverManager.getConnection( infoConnection, user, pwd ) ;
+    }
+
+    @Override
+    public void close() {
+        try{
+            dbConnection.close();
+        }
+        catch(SQLException e){
+            System.err.println(e.getMessage());
+        }
+    }
+
+    @Override
+    public Order getOrder(String id) {
+
+        Order selectedOrder = null;
+
+        String query = "SELECT * FROM Order WHERE id=? LEFT JOIN Order_content ON Order.id = Order_content.Order_id";
+
+        // construction et exécution d'une requête préparée
+        try ( PreparedStatement ps = dbConnection.prepareStatement(query) ){
+            ps.setString(1, id);
+
+            // exécution de la requête
+            ResultSet result = ps.executeQuery();
+            if (result.getFetchSize() == 0) {
+                return selectedOrder;
+            }
+
+            String[] cartId = new String[result.getFetchSize()];
+            String tmp = "";
+            int index = 0;
+
+            // récupération des tuples résultats
+            // (si l'identifiant de la commande est valide)
+            while ( result.next() )
+            {
+                tmp = result.getString("cart_id");
+                if (tmp != null) {
+                    cartId[index] = tmp;
+                    ++index;
+                }
+            }
+
+            result.previous();
+
+            String userId = result.getString("user_id");
+            String relayAddress = result.getString("relay_address");
+            String date = result.getString("limit_date");
+            boolean valid = result.getBoolean("is_validate");
+
+            selectedOrder = new Order(id, userId, date, relayAddress, valid);
+            selectedOrder.setCartId(cartId);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return selectedOrder;
+    }
+
+    @Override
+    public ArrayList<Order> getAllOrders() {
+        ArrayList<Order> listOrders = new ArrayList<>();
+
+        String query = "SELECT * FROM Order WHERE id=? LEFT JOIN Order_content ON Order.id = Order_content.Order_id ORDER BY id";
+
+        // construction et exécution d'une requête préparée
+        try ( PreparedStatement ps = dbConnection.prepareStatement(query) ){
+
+            // exécution de la requête
+            ResultSet result = ps.executeQuery();
+            if (result.getFetchSize() == 0) {
+                return listOrders;
+            }
+
+            String[] cartId = new String[result.getFetchSize()];
+            String tmp = "";
+            String currentOrderId = "";
+            int index = 0;
+
+            // récupération des tuples résultats
+            // (si l'identifiant de la commande est valide)
+            while ( result.next() )
+            {
+                tmp = result.getString("cart_id");
+                if (tmp != null) {
+                    if (!tmp.equals(currentOrderId)) {
+                        result.previous();
+                        String id = result.getString("id");
+                        String userId = result.getString("user_id");
+                        String relayAddress = result.getString("relay_address");
+                        String date = result.getString("limit_date");
+                        boolean valid = result.getBoolean("is_validate");
+                        Order currentOrder = new Order(id, userId, relayAddress, date, valid);
+                        currentOrder.setCartId(Arrays.copyOfRange(cartId, 0, index));
+                        listOrders.add(currentOrder);
+                        index = 0;
+                        result.next();
+                    }
+                    cartId[index] = tmp;
+                    ++index;
+                }
+            }
+
+            result.previous();
+            String id = result.getString("id");
+            String userId = result.getString("user_id");
+            String relayAddress = result.getString("relay_address");
+            String date = result.getString("limit_date");
+            boolean valid = result.getBoolean("is_validate");
+            Order currentOrder = new Order(id, userId, relayAddress, date, valid);
+            currentOrder.setCartId(Arrays.copyOfRange(cartId, 0, index));
+            listOrders.add(currentOrder);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return listOrders;
+    }
+
+    @Override
+    public boolean updateOrder( String id, String userId, String date, String relayAddress, boolean valid) {
+        String query = "UPDATE Order SET user_id=?, limit_date=?, relay_address=?, is_validate=? where id=?";
+        int nbRowModified = 0;
+
+        // construction et exécution d'une requête préparée
+        try ( PreparedStatement ps = dbConnection.prepareStatement(query) ){
+            ps.setString(1, userId);
+            ps.setString(2, date);
+            ps.setString(3, relayAddress );
+            ps.setString(4, String.valueOf(valid));
+            ps.setString(5, id);
+
+            // exécution de la requête
+            nbRowModified = ps.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return ( nbRowModified != 0 );
+    }
+}
